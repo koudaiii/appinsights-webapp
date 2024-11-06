101e350b2712a133f4596ddf01a677f0b5a00fec
koudaiii <cs006061@gmail.com>
2024-11-06 10:33:56 +0900

    Enable Application Insights

--- Pages/Index.cshtml.cs
+++ Pages/Index.cshtml.cs
@@ -1,3 +1,4 @@
+using Microsoft.ApplicationInsights;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
@@ -5,15 +6,21 @@ namespace appinsights_webapp.Pages;
 
 public class IndexModel : PageModel
 {
+    private readonly TelemetryClient _tc;
     private readonly ILogger<IndexModel> _logger;
 
-    public IndexModel(ILogger<IndexModel> logger)
-    {
-        _logger = logger;
-    }
+    public IndexModel(ILogger<IndexModel> logger, TelemetryClient tc ) => (_logger, _tc) = (logger, tc);
 
     public void OnGet()
     {
-
+        _tc.TrackEvent("IndexModel_OnGet", new Dictionary<string, string> { { "Page", "Index" } });
+        try
+        {
+            throw new NullReferenceException("This is a test exception");
+        }
+        catch (Exception e)
+        {
+            _logger.LogError(e, "An error occurred in the IndexModel_OnGet method");
+        }
     }
 }
